365c96473035f561a16062bc5dbb9b4d56b8504a
Cryonix <cryonixsup@proton.me>
2026-04-08 18:40:51 +0300

    Create arithmetic.c

---
+++ examples/arithmetic.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void main(void);
+
+void main(void) {
+    int x = 10;
+    int y = 20;
+    int sum = (x + y);
+    printf("%d\n", sum);
+    int counter = 0;
+    (counter = (counter + 1));
+    printf("%d\n", counter);
+}
